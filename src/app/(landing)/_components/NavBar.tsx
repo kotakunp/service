fdd7ad3c9a966338c7ce09ecd41e682632074dc2
kotakunp <kotakunp@192.168.1.5>
2025-09-04 21:57:25 +0800

    navbar clerk

--- src/app/(landing)/_components/NavBar.tsx
+++ src/app/(landing)/_components/NavBar.tsx
@@ -5,7 +5,7 @@ import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs"
 export function NavBar() {
   return (
     <header className="flex py-6 shadow-xl fixed top-0 w-full z-10 bg-background/95">
-      <nav className="flex items-center gap-10 container font-semibold">
+      <nav className="flex items-center gap-10 font-semibold">
         <Link href="/" className="mr-auto">
           <BrandLogo />
         </Link>
