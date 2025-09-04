d26636d4a0114764c65ac91d785e553b8783609c
kotakunp <kotakunp@192.168.1.5>
2025-09-04 21:46:53 +0800

    navbar and clerk

---
+++ src/app/(landing)/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+import { NavBar } from "./_components/NavBar";
+
+export default function LandingLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="selection:bg-pink-200">
+        <NavBar />
+        teststststs
+        {children}
+    </div>
+  )
+}       
---
+++ src/app/components/BrandLogo.tsx
@@ -0,0 +1,10 @@
+import { FileMusic } from "lucide-react"
+
+export function BrandLogo() {
+    return (
+        <span className="flex items-center gap-2 font-semibold flex-shrink-0 text-lg mr-auto">
+            <FileMusic className="size-8"/>
+            <span>EzMuzik</span>
+        </span>
+    )
+} 
