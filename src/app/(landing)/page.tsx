508a467a08a10f9ef7a8ef469908397692c3b1f9
kotakunp <kotakunp@192.168.1.5>
2025-09-06 21:21:56 +0800

    pricing section and cards

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,8 +1,13 @@
 import { SignUpButton } from "@clerk/nextjs";
-import { ArrowRightIcon } from "lucide-react";
+import { ArrowRightIcon, CheckIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import TestIcon from "./_icons/logo";
+import { pricingTiersInOrder } from "@/data/priceTiers";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { formatCompactNumber } from "@/lib/formatters";
+import { ReactNode } from "react";
+import { cn } from "@/lib/utils";
 
 export default function HomePage() {
   return (
@@ -21,11 +26,14 @@ export default function HomePage() {
         </SignUpButton>
       </section>
       <section className="bg-primary text-primary-foreground">
-        <div className="container py-16 flex flex-col gap-16 px-8 md:px-16">
+        <div className="py-16 flex flex-col gap-16 px-8 md:px-16">
           <h2 className="text-3xl text-center text-balance">
             Second section text
           </h2>
-          <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-16">
+          <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-16">
+            <Link href="https://youtube.com/@kotakunp">
+              <TestIcon />
+            </Link>
             <Link href="https://youtube.com/@kotakunp">
               <TestIcon />
             </Link>
@@ -53,6 +61,69 @@ export default function HomePage() {
           </div>
         </div>
       </section>
+      <section id="pricing" className="px-8 py-16 bg-accent/5">
+        <h2 className="text-4xl text-center text-balance font-semibold mb-8">
+          Just a pricing section text that i will change later
+        </h2>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 max-w-screen-xl mx-auto">
+          {pricingTiersInOrder.map(tier => (
+            <PricingCard key={tier.name} {...tier} />
+          ))}
+        </div>
+      </section>
     </>
   )
 }
+
+function PricingCard ({
+  name,
+  priceInCents,
+  maxNumberOfVisits,
+  maxNumberOfProducts,
+  canRemoveBranding,
+  canAccessAnalytics,
+  canCustomizeBanner,
+}: (typeof pricingTiersInOrder)[number]) {
+  const isMostPopular = name === "Standart"
+
+
+  return (
+    <Card>
+      <CardHeader>
+        <div className="text-accent font-semibold mb-8">{name}</div>
+        <CardTitle>${priceInCents / 100} /mo</CardTitle>
+        <CardDescription>
+          {formatCompactNumber(maxNumberOfVisits)} pricing page visits/mo
+          </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <SignUpButton>
+          <Button className="text-lg w-full rounded-lg"
+                  variant={isMostPopular ? "destructive" : "default"}
+          >
+            Get Started
+          </Button>
+        </SignUpButton>
+      </CardContent>
+      <CardFooter className="flex flex-col gap-4 items-start">
+        <Feature className="font-bold">
+          {maxNumberOfProducts}{" "}
+          {maxNumberOfProducts === 1 ? "product" : "products"}
+        </Feature>
+        {canAccessAnalytics && <Feature> Analytics access</Feature>}
+        {canCustomizeBanner && <Feature> Banner customization</Feature>}
+        {canRemoveBranding && <Feature> Removable EzMuzik branding</Feature>}
+      </CardFooter>
+    </Card>
+  )
+}
+
+function Feature({ children, className }: { children: ReactNode, className?: string}) {
+  return (
+    <div className={cn("flex items-center gap-2", className)}>
+      <CheckIcon className="size-4 stroke-accent bg-accent/25 rounded-full p-0.5" />
+      <span>{children}</span>
+    </div>
+  )
+}
+
