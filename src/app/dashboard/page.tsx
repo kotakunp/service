9cf7e5d3247c2cf38df1cb74246842880192fc39
kotakunp <kuroko.tuyabayar77@gmail.com>
2025-09-07 15:46:43 +0800

    drizzle setup

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
 export default function DashboardPage() {
-    return null
+    return (
+        <div></div>
+    )
 }
---
+++ src/drizzle/db.ts
@@ -0,0 +1,9 @@
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { env } from '@/data/env/server';
+import * as schema from "./schema"
+
+const sql = neon(env.DATABASE_URL);
+export const db = drizzle(sql, { schema });
+
+const result = await db.execute('select 1');
