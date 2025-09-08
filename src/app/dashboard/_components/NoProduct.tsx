f802bb4a23827b07aef2e88e2e36fae77c035ca3
kotakunp <kuroko.tuyabayar77@gmail.com>
2025-09-08 16:02:45 +0800

    some debugging 2

--- src/app/dashboard/_components/NoProduct.tsx
+++ src/app/dashboard/_components/NoProduct.tsx
@@ -3,14 +3,16 @@ import Link from "next/link";
 
 export function NoProducts() {
   return (
-    <div className="mt-32 text-center text-balance">
-      <h1 className="text-4xl font-semibold mb-2">You have no products</h1>
-      <p className="mb-4">
-        Get started with PPP discounts by creating a product
-      </p>
-      <Button size="lg" asChild>
-        <Link href="/dashboard/products/new">Add Product</Link>
-      </Button>
+    <div className="flex w-screen min-h-[60vh] items-center justify-center">
+      <div className="flex flex-col items-center text-center">
+        <h1 className="text-4xl font-semibold mb-2">You have no products</h1>
+        <p className="mb-4">
+          Get started with PPP discounts by creating a product
+        </p>
+        <Button size="lg" asChild>
+          <Link href="/dashboard/products/new">Add Product</Link>
+        </Button>
+      </div>
     </div>
   );
 }
